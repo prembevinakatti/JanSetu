2a97c287764ce7c02ac993eeddd577780b127cb5
OnkarBevinakatti <onkarbevinakatti09@gmail.com>
2026-02-05 13:44:01 +0530

    ..

---
+++ server/models/auth.model.js
@@ -0,0 +1,27 @@
+const { default: mongoose } = require("mongoose");
+
+const authSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["User", "Admin"],
+    default: "user",
+  },
+  metamaskId: {
+    type: String,
+  },
+});
+
+
