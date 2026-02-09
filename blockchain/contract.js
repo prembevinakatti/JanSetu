53154dd1efe188ef3fbf83e8992ffb7d0e277bb0
OnkarBevinakatti <onkarbevinakatti09@gmail.com>
2026-02-09 15:41:26 +0530

    ..

--- blockchain/contract.js
+++ blockchain/contract.js
@@ -1,20 +1,37 @@
-const { ethers } = require("ethers");
-const contractABI = require("./artifacts/contracts/CIvicChain.sol/CivicChain.json");
+import { ethers } from "ethers";
+import dotenv from "dotenv";
+dotenv.config();
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// Provider (Polygon Amoy)
+/* Fix __dirname in ES modules */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/* Load ABI safely */
+const artifactPath = path.join(
+  __dirname,
+  "artifacts",
+  "contracts",
+  "CivicChain.sol",
+  "CivicChain.json",
+);
+
+console.log(process.env.PRIVATE_KEY);
+console.log(process.env.PRIVATE_KEY?.length);
+
+
+const contractJSON = JSON.parse(fs.readFileSync(artifactPath, "utf8"));
+const abi = contractJSON.abi;
+
+/* Provider (Polygon Amoy) */
 const provider = new ethers.JsonRpcProvider(process.env.AMOY_RPC);
 
-// Wallet (Admin / System wallet)
-const wallet = new ethers.Wallet(
-  process.env.PRIVATE_KEY,
-  provider
-);
+/* Wallet (Admin / System wallet) */
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
-// Contract instance
-const contract = new ethers.Contract(
-  process.env.CONTRACT_ADDRESS,
-  contractABI,
-  wallet
-);
+/* Contract instance */
+const contract = new ethers.Contract(process.env.CONTRACT_ADDRESS, abi, wallet);
 
-module.exports = contract;
+export default contract;
