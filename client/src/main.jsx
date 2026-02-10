833ff08a31a82cd1c8ff667cc4b8bbe8e28b65ad
OnkarBevinakatti <onkarbevinakatti09@gmail.com>
2026-02-10 16:08:19 +0530

    ..

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import "leaflet/dist/leaflet.css";
+import "@/utils/leafletIconFix";
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
